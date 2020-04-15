dab19136f51a9a382b96da9858c784b1eb1921b0
jvalley19 <52843322+jvalley19@users.noreply.github.com>
2020-04-15 08:36:29 -0400

    231

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -l
 
-#echo "Hi dummy"
-#echo "Hello $1"
-#time=$(hon)
-#help='help'
-#echo "::set-output name=time::$help"
+echo "Hi dummy"
+echo "Hello $1"
+time=$(date)
+help='help'
+echo "::set-output name=etime::$time"
 #file=$(ls)
 #echo "::set-output name=ls::$file"
